1cb221b395217c27d1552b5d161b7c73faf43511
Herri Siagian <herry.it.2007@gmail.com>
2020-04-07 14:48:37 +0700

    menambahkan perkenalan dart

---
+++ perkenalan/hello_challange.dart
@@ -0,0 +1,16 @@
+/*
+ * www.seredata.com 
+ * Belajar Pemrograman Dart
+ * ===================================================
+ * author    : nama kamu
+ * nama file : hello_challange.dart
+ */
+
+/* TODO
+  buatlah kode untuk menampilkan biodata singkat
+  nama : <nama kamu>
+  alamat : <alamat kamu>
+  tempat lahir : <tempat lahir>
+*/
+
+// tulis kode program dibawah ini.
---
+++ perkenalan/hello_final.dart
@@ -0,0 +1,11 @@
+/*
+ * www.seredata.com 
+ * Belajar Pemrograman Dart
+ * ===================================================
+ * author    : herri siagian <herry.it.2007@gmail.com>
+ * nama file : hello_final.dart
+ */
+
+void main() {
+  print("Hello, World!");
+}
