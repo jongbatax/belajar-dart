074b060cfa5ed0961332e5e1334ed23cdaf46540
Herri Siagian <herry.it.2007@gmail.com>
2020-04-14 22:27:55 +0700

    Format source variabel_challange2.dart

--- variabel-tipedata/variabel_challange2.dart
+++ variabel-tipedata/variabel_challange2.dart
@@ -9,18 +9,15 @@
  */
 
 //TODO gunakan library dari dart untuk melakukan pemangkatan.
-
 // <code disini>
 
 void main() {
   //TODO deklarasikan variabel dengan nama bilangan, dengan tipe Integer.
-
   // <code disini>
 
   // TODO cetak kuadart dari bilangan tersebut.
   // petunjuk pencetakan.
   // print("Kuadart dari $namaVariabel = ${namaLibraryPangkat}");
   // library yang kita gunakan adalah pow(base,pangkat)
-
   // <code disini>
 }
